5836a6d694f9dd25545dfb3dbb68a3169512636b
Nandhini S <sainandhu1998@gmail.com>
2023-08-15 21:18:26 +0530

    clcik event with parameter

--- src/Content.js
+++ src/Content.js
@@ -11,12 +11,19 @@ const Content = () => {
     console.log("have a happy day");
   };
 
+  const HandleClickParam = (name) => {
+    console.log(`have a happy day ${name}`);
+  };
+
   return (
     <header>
       <h1>i am from content component</h1>
       <p> Let's {HandleNamechange()} Love</p>
 
       <button onClick={HandleClick}>Please click here</button>
+      <button onClick={() => HandleClickParam("kutty")}>
+        Please click here too
+      </button>
     </header>
   );
 };
