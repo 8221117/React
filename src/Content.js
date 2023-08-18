b9bd39d02e42c5a05843b85bfcb4296c4c8238ba
Nandhini S <sainandhu1998@gmail.com>
2023-08-19 00:07:11 +0530

    click button to change the data

--- src/Content.js
+++ src/Content.js
@@ -2,78 +2,19 @@ import React from "react";
 import { useState } from "react";
 
 const Content = () => {
-  function HandleNamechange() {
+  function HandleNamechange2() {
     const names = ["earn", "grow", "give"];
     const matchNames = Math.floor(Math.random() * 3);
-    return names[matchNames];
+    setRandomName(names[matchNames]);
   }
 
-  const HandleClick = () => {
-    console.log("have a happy day");
-  };
-
-  const HandleClickParam = (name) => {
-    console.log(`have a happy day ${name}`);
-  };
-
-  const toCheckevent = (e) => {
-    console.log(e.target);
-    console.log(e.target.innerText);
-  };
-
-  const [count, setCount] = useState(1);
-
-  const [name, setName] = useState({
-    name: "nandhu",
-    text: "like",
-  }); /* don't use object inside useState , if you still want to use then , give one value ina use state like text:"like" */
-
-  function incrementFunction() {
-    setCount((prevCount) => {
-      return prevCount + 1;
-    });
-    setCount((count) => {
-      return count + 1;
-    });
-    setCount((count) => {
-      return count + 1;
-    }); /* corrected version of function to increment by three Times */
-  }
-
-  function decrementFunction() {
-    setCount(count - 1);
-  }
-
-  function Name() {
-    return console.log(
-      "this is right way . This functions is called only once when the page reloads"
-    );
-  }
+  const [randomName, setRandomName] = useState("Earn");
 
   return (
     <header>
-      <h1>i am from content component</h1>
-      <p> Let's {HandleNamechange()} Love</p>
-      <button onClick={HandleClick}>Please click here</button>
-      <button onClick={() => HandleClickParam("kutty")}>
-        Please click here too
-      </button>
-
-      {/* here we are using anonymous function to give the parameter so that
-      function executes only when we click */}
-
-      <button onClick={(e) => toCheckevent(e)}>subscribe</button>
-      {/*  here we use event.target to check which element was clicked  */}
-
-      <button onDoubleClick={() => HandleClickParam("nandhu")}>
-        Double Click this Button
-      </button>
-      <br />
-      <button onClick={decrementFunction}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementFunction}>+</button>
+      <p> Let's {randomName} Money</p>
+      <button onClick={HandleNamechange2}>Click Here</button>
     </header>
   );
 };
-
 export default Content;
