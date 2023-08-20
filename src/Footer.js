6ac5729db07818e4c8287e4ab82c4984f83d388d
Nandhini S <sainandhu1998@gmail.com>
2023-08-20 20:33:37 +0530

    to get length of the items in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
-const Footer = () => {
-  const year = new Date();
+const Footer = ({ length }) => {
+  /* const year = new Date(); */
   return (
     <>
-      <footer>CopyRight &copy; {year.getFullYear()}</footer>
+      {/* <footer>CopyRight &copy; {year.getFullYear()}</footer> */}
+      <footer>
+        {length} List {length === 1 ? "item" : "items"}
+      </footer>
       {/* <h1>i am from Footer component</h1> */}
     </>
   );
