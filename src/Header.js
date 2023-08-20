bae1f1aa740e46b8bbf74832a11e543db31f43bf
Nandhini S <sainandhu1998@gmail.com>
2023-08-20 20:02:42 +0530

    destructuring directly instead of props

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-const Header = (props) => {
+const Header = ({ title }) => {
   const headerStyle = { backgroundColor: "violet", color: "white" };
   return (
     <header style={headerStyle}>
       {/* <header style={{ backgroundColor: "violet", color: "white" }}> */}
       {/* <h1>i am from header component</h1> */}
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
