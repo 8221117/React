b68f7a643355e9f99b01cd589f045f43f30db9f7
Nandhini S <sainandhu1998@gmail.com>
2023-08-20 17:49:10 +0530

    style label line through

--- src/Content.js
+++ src/Content.js
@@ -38,7 +38,10 @@ const Content = () => {
               onChange={() => handleCheck(item.id)}
               checked={item.checked}
             ></input>
-            <label onDoubleClick={() => handleCheck(item.id)}>
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
               {item.item}
             </label>
             <FaTrashAlt
