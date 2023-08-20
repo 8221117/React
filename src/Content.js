a2fbbe0af92ea172b4a538e92f40f4b740e579ef
Nandhini S <sainandhu1998@gmail.com>
2023-08-20 17:55:18 +0530

    Show message if entire list is empty -items.length

--- src/Content.js
+++ src/Content.js
@@ -30,29 +30,33 @@ const Content = () => {
 
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            ></input>
-            <label
-              style={item.checked ? { textDecoration: "line-through" } : null}
-              onDoubleClick={() => handleCheck(item.id)}
-            >
-              {item.item}
-            </label>
-            <FaTrashAlt
-              role="button"
-              tabIndex="0"
-              onClick={() => handleDelete(item.id)}
-            ></FaTrashAlt>
-            <FaEdit role="button" tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              ></input>
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                role="button"
+                tabIndex="0"
+                onClick={() => handleDelete(item.id)}
+              ></FaTrashAlt>
+              <FaEdit role="button" tabIndex="0" />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Your LIST is EMPTY</p>
+      )}
     </main>
   );
 };
