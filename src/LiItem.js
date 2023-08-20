0b8f8c75bd952f53316ebc2f468ee12789b22af5
Nandhini S <sainandhu1998@gmail.com>
2023-08-20 22:36:00 +0530

    creating separate component for li - LiItem.js

---
+++ src/LiItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      ></input>
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        role="button"
+        tabIndex="0"
+        onClick={() => handleDelete(item.id)}
+      ></FaTrashAlt>
+      <FaEdit role="button" tabIndex="0" />
+    </li>
+  );
+};
+
+export default LineItem;
--- src/List.js
+++ src/List.js
@@ -1,30 +1,17 @@
 import React from "react";
 
-import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import LineItem from "./LiItem";
 
 const Itemslist = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li className="item" key={item.id}>
-          <input
-            type="checkbox"
-            onChange={() => handleCheck(item.id)}
-            checked={item.checked}
-          ></input>
-          <label
-            style={item.checked ? { textDecoration: "line-through" } : null}
-            onDoubleClick={() => handleCheck(item.id)}
-          >
-            {item.item}
-          </label>
-          <FaTrashAlt
-            role="button"
-            tabIndex="0"
-            onClick={() => handleDelete(item.id)}
-          ></FaTrashAlt>
-          <FaEdit role="button" tabIndex="0" />
-        </li>
+        <LineItem
+          item={item}
+          key={item.id}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ))}
     </ul>
   );
